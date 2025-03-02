390a041c279948fa7ae8143a7977863473f94747
Chris Roberts <git@chobert.org>
2025-03-03 00:49:29 +0100

    Forgot to add file in previous commit

---
+++ src/syntax.lisp
@@ -0,0 +1,42 @@
+(defconstant +lisp-form-character+ #\$)
+(defconstant +single-escape-character+ #\\)
+
+(defconstant +whitespace-vertical+ '(#\Space #\Tab))
+(defconstant +whitespace-horizontal+ '(#\Newline))
+(defconstant +whitespace+ (append +whitespace-vertical+ +whitespace-horizontal+))
+
+(defconstant +sexpcode-starting-character+ #\{)
+(defconstant +sexpcode-ending-character+ #\})
+(defconstant +sexpcode-class-separating-character+ #\.)
+(defconstant +sexpcode-id-separating-character+ #\#)
+(defconstant +sexpcode-attribute-list-starting-character+ #\[)
+(defconstant +sexpcode-attribute-list-ending-character+ #\])
+(defconstant +sexpcode-tag-ending-character+ #\;)
+(defconstant +sexpcode-tag-nesting-character+ #\,)
+
+(defconstant +sexpcode-tag-terminating-characters+
+  (list +sexpcode-tag-ending-character+
+        +sexpcode-tag-nesting-character+))
+
+(defconstant +sexpcode-name-ending-characters+
+  (append (list +sexpcode-starting-character+
+                +sexpcode-ending-character+
+                +sexpcode-class-separating-character+
+                +sexpcode-id-separating-character+
+                +sexpcode-attribute-list-starting-character+)
+          +sexpcode-tag-terminating-characters+
+          +whitespace+))
+
+(defconstant +sexpcode-class-ending-characters+
+  +sexpcode-name-ending-characters+)
+
+(defconstant +sexpcode-id-ending-characters+
+  (remove +sexpcode-class-separating-character+
+          +sexpcode-class-ending-characters+))
+
+(defconstant +sexpcode-attribute-name-invalid-characters+
+  (list #\/ #\> #\=))
+(defconstant +sexpcode-attribute-name-ending-characters+
+  (append (list #\= #\]) +whitespace+))
+(defconstant +sexpcode-attribute-unquoted-ending-characters+
+  (append +whitespace+ '(#\])))
